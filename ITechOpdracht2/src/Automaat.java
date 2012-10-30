de848158cddffa7538f6e821774ebf13da3b4b28
Robin Verheijen <robin.verheijen@hotmail.com>
2012-10-30 12:40:40 +0100

    handshake

--- ITechOpdracht2/src/Automaat.java
+++ ITechOpdracht2/src/Automaat.java
@@ -6,38 +6,71 @@ import java.net.Socket;
 
 public class Automaat {
 
-	private int id;
+	private static final int id = 1;
 	private String address;
 	
-	public Automaat(int theId, String theAddress)	{
-		id = theId;
-		address = theAddress;
-	}
 	public static void main(String[] args) throws Exception {
 		
 		String sentence;
 		
+		String pasnummer;
+		
+		System.out.println("Vul uw pasnummer in: ");
 		BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
 		Socket clientSocket = new Socket("localhost",8080);
 		DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
 		BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
 		
-		sentence = inFromUser.readLine();
-		outToServer.writeBytes(sentence + "\n");
-		System.out.println(inFromServer.readLine());
-		clientSocket.close();
+		//pasnummer uitlezen
+		pasnummer = inFromUser.readLine();
 		
-	}
-	public int getId() {
-		return id;
-	}
-	public void setId(int id) {
-		this.id = id;
-	}
-	public String getAddress() {
-		return address;
-	}
-	public void setAddress(String address) {
-		this.address = address;
+		System.out.println(pasnummer);
+		//voordat het pasnummer wordt opgestuurd de handshake uitvoeren
+		
+		//vragen aan de bank of het wel de bank is
+		outToServer.writeBytes("bankcontrole" + "\n");
+		
+		//reactie van de bank uitlezen, als het het juiste id is stuurt de automaat de informatie op
+				
+		sentence = inFromServer.readLine();
+		String[] bankcheck = sentence.split(" ");
+		
+		System.out.println(bankcheck[0]);
+		System.out.println(bankcheck[1]);
+
+		if(bankcheck[0].equals("bankid"))	{
+			if(bankcheck[1].equals("2"))	{
+				outToServer.writeBytes("automaatid "+ id + "\n");
+			}
+		}
+		
+		String authenticatie = inFromServer.readLine();
+		System.out.println("AUTHENTICATIE" + authenticatie);
+		
+		if(authenticatie.equals("1"))	{
+			System.out.println("authenticatie correct");
+			outToServer.writeBytes("pasnummer " + pasnummer + "\n");
+		} else if(authenticatie.equals("2"))	{
+			System.out.println("authenticatie fout");
+		}
+		
+		String[] pasnummerAuthenticatie = inFromServer.readLine().split(" ");
+		
+		System.out.println(pasnummerAuthenticatie[0]);
+		System.out.println(pasnummerAuthenticatie[1]);
+		
+		if(pasnummerAuthenticatie[0].equals("pasnummer"))	{
+			if(pasnummerAuthenticatie[1].equals("1"))	{
+				System.out.println("pincode: ");
+			} else	{
+				System.out.println("fout pasnummer, de verbinding wordt verbroken");
+				clientSocket.close();
+			}
+		}
+
+		
+		
+		
+		
 	}
 }
--- ITechOpdracht2/src/Bank.java
+++ ITechOpdracht2/src/Bank.java
@@ -6,6 +6,8 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Bank {
+	
+	private static final int id = 2;
 
 	public static void main(String argv[]) throws Exception {
 		
@@ -17,15 +19,52 @@ public class Bank {
 			
 			BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
 			
+			PrintWriter writer = new PrintWriter(connectionSocket.getOutputStream(), true);
+
+			//Uitlezen wat de automaat heeft opgestuurd
+			String bankcontrole = inFromClient.readLine();
 			
-			clientSentence = inFromClient.readLine();
+			System.out.println(bankcontrole);
+			//Als de automaat om een "bankcontrole" heeft gevraagd stuurt de bank zijn id terug
+			if(bankcontrole.equals("bankcontrole"))	{
+				writer.println("bankid "+ id);
+			}
 			
-			System.out.println(clientSentence);
+			//Als het id van de bank is goedegekeurd stuurt de automaat zijn id op
+			String automaatcontrole = inFromClient.readLine();
+
+			String[] sentence = automaatcontrole.split(" ");
+			
+			System.out.println(sentence[0]);
+			System.out.println(sentence[1]);
+			if(sentence[0].equals("automaatid"))	{
+				if(sentence[1].equals("1"))	{
+					clientSentence = "1";
+					writer.println("1");
+				} else	{
+					clientSentence = "2";
+					writer.println("2");
+				}
+			}
+			
+			String pasnummer = inFromClient.readLine();
+			
+			System.out.println(pasnummer);
+			String[] automaatpasnummer = pasnummer.split(" ");
+			
+			System.out.println(automaatpasnummer[0]);
+			System.out.println(automaatpasnummer[1]);
+			
+			if(automaatpasnummer[0].equals("pasnummer"))	{
+				if(!automaatpasnummer[1].equals("123456789"))	{
+					writer.println("pasnummer " + 1);
+				}
+			}
+			
 			
 //			DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
 
-			PrintWriter writer = new PrintWriter(connectionSocket.getOutputStream(), true);
-			writer.println(clientSentence.toUpperCase());
+
 //			outToClient.writeBytes(clientSentence.toUpperCase());
 			
 			//connectionSocket.close();
