b64b4a4eafa27e6431b5a0f7fd460bca197317a0
Robin Verheijen <robin.verheijen@hotmail.com>
2012-10-30 11:47:04 +0100

    outToClient PrintWriter

--- ITechOpdracht2/src/Bank.java
+++ ITechOpdracht2/src/Bank.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -13,13 +14,21 @@ public class Bank {
 		ServerSocket welcomeSocket = new ServerSocket(8080);
 		while(true) {
 			Socket connectionSocket = welcomeSocket.accept();
+			
 			BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
-			DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
+			
+			
 			clientSentence = inFromClient.readLine();
 			
 			System.out.println(clientSentence);
 			
-			outToClient.writeBytes(clientSentence.toUpperCase());
+//			DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
+
+			PrintWriter writer = new PrintWriter(connectionSocket.getOutputStream(), true);
+			writer.println(clientSentence.toUpperCase());
+//			outToClient.writeBytes(clientSentence.toUpperCase());
+			
+			//connectionSocket.close();
 		}
 		
 	}
