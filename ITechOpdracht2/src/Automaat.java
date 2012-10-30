9ceb43d17d9a58e8c8681d6f27d52560b4cc0105
Saron <iphone.grave@gmail.com>
2012-10-30 11:15:04 +0100

    basic automaat en bank

--- ITechOpdracht2/src/Automaat.java
+++ ITechOpdracht2/src/Automaat.java
@@ -1,3 +1,8 @@
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
 
 public class Automaat {
 
@@ -9,6 +14,18 @@ public class Automaat {
 		address = theAddress;
 	}
 	public static void main(String[] args) throws Exception {
+		
+		String sentence;
+		
+		BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
+		Socket clientSocket = new Socket("localhost",8080);
+		DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
+		BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+		
+		sentence = inFromUser.readLine();
+		outToServer.writeBytes(sentence + "\n");
+		System.out.println(inFromServer.readLine());
+		clientSocket.close();
 		
 	}
 	public int getId() {
--- ITechOpdracht2/src/Bank.java
+++ ITechOpdracht2/src/Bank.java
@@ -1,4 +1,27 @@
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 public class Bank {
 
+	public static void main(String argv[]) throws Exception {
+		
+		String clientSentence;
+		
+		ServerSocket welcomeSocket = new ServerSocket(8080);
+		while(true) {
+			Socket connectionSocket = welcomeSocket.accept();
+			BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
+			DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
+			clientSentence = inFromClient.readLine();
+			
+			System.out.println(clientSentence);
+			
+			outToClient.writeBytes(clientSentence.toUpperCase());
+		}
+		
+	}
+	
 }
