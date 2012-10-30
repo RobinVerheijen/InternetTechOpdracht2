087c969c04bea201afe981fb9332e9d18f7be553
Robin Verheijen <robin.verheijen@hotmail.com>
2012-10-30 10:34:20 +0100

    automaat

--- ITechOpdracht2/src/Automaat.java
+++ ITechOpdracht2/src/Automaat.java
@@ -1,7 +1,27 @@
 
 public class Automaat {
 
+	private int id;
+	private String address;
+	
+	public Automaat(int theId, String theAddress)	{
+		id = theId;
+		address = theAddress;
+	}
 	public static void main(String[] args) throws Exception {
 		
 	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
 }
